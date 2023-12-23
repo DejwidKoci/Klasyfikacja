c2caff7647067dfa9ad31dd8410c9d94c9c21ccd
DejwidKoci <s205915@sggw.edu.pl>
2023-12-23 15:54:09 +0100

    pca

---
+++ visualize_training_data.py
@@ -0,0 +1,70 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_excel("hour.xlsx")
+y = pd.read_excel("hour.xlsx")['season']
+X = data.drop(columns = ['instant', 'dteday', 'season'])
+
+X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size = 0.2, 
+                     stratify = y,
+                     random_state = 0)
+
+# standardize the features
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
+
+
+
+# covariance matrix
+cov_mat = np.cov(X_train_std.T)
+eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+print('\nEigenvalues \n', eigen_vals)
+
+
+
+# Total and explained variance
+
+tot = sum(eigen_vals)
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse = True)]
+cum_var_exp = np.cumsum(var_exp)
+
+plt.bar(range(1, 15), var_exp, align = 'center', label = 'Individual explained variance')
+plt.step(range(1, 15), cum_var_exp, where = 'mid', label = 'Cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal component index')
+plt.legend(loc = 'best')
+plt.tight_layout()
+plt.show()
+
+
+# Feature transformation
+## Make a list of eigenvalue, eigenvector) tuples
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in range(len(eigen_vals))]
+
+## Sort the (eigenvalue, eigenvector) tuples from high to low
+eigen_pairs.sort(key = lambda k: k[0], reverse = True)
+
+## Matrix W
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
+               eigen_pairs[1][1][:, np.newaxis]))
+print('Matrix W: \n', w)
+print(X_train_std[0].dot(w))
+
+## Visualize training dataset
+X_train_pca = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['o', 's', '^']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train == l, 0],
+                X_train_pca[y_train == l, 1],
+                c = c, label = f'Class {l}', marker = m)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc = 'lower left')
+plt.tight_layout()
+plt.show()
