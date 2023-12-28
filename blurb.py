49cdc33c34746d9124eefcd9cf35ad8763f77517
Dejwid <s205915@sggw.edu.pl>
2023-12-28 22:35:41 +0100

    dane

--- blurb.py
+++ blurb.py
@@ -1,54 +1,25 @@
 import pandas as pd
-# df = pd.read_csv('https://archive.ics.uci.edu/ml/'
-#                  'machine-learning-databases'
-#                  '/breast-cancer-wisconsin/wdbc.data',
-#                  header=None)
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+wine_quality = fetch_ucirepo(id=186) 
+  
+# data (as pandas dataframes) 
+X = wine_quality.data.features 
+y = wine_quality.data.targets 
+  
+# metadata 
+print(wine_quality.metadata) 
+  
+# variable information 
+print(wine_quality.variables) 
 
 
 
-from sklearn.preprocessing import LabelEncoder
-data = pd.read_excel("hour.xlsx", header = None)
-# y = data['season']
-# X = data.drop(columns = ['instant', 'dteday', 'season'])
+# Zapis do pliku Excel
+data_combined = pd.concat([X, y], axis=1)  # Łączenie cech i etykiet
 
-y = data.loc[1:, 2].values
-X = data.loc[1:, 3:].values
+excel_file_path = 'wine.xlsx'  # Nazwa pliku Excel
 
-
-# X = df.loc[:, 2:].values
-# y = df.loc[:, 1]. values
-print(X)
-print(y)
-le = LabelEncoder()
-y = le.fit_transform(y)
-print(le.classes_)
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size = 0.2, stratify = y, random_state = 1)
-
-
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import make_pipeline
-pipe_lr = make_pipeline(StandardScaler(),
-                        PCA(n_components = 2),
-                        LogisticRegression())
-pipe_lr.fit(X_train, y_train)
-y_pred = pipe_lr.predict(X_test)
-test_acc = pipe_lr.score(X_test, y_test)
-print(f'Test accuracy: {test_acc: .3f}')
-
-
-import numpy as np
-from sklearn.model_selection import StratifiedKFold
-kfold = StratifiedKFold(n_splits = 10).split(X_train, y_train)
-scores = []
-for k, (train, test) in enumerate(kfold):
-    pipe_lr.fit(X_train[train], y_train[train])
-    score = pipe_lr.score(X_train[test], y_train[test])
-    scores.append(score)
-    print(f'Fold: {k+1:02d}, '
-          f'Class distr.: {np.bincount(y_train[train])}, '
-          f'Acc.: {score:.3f}')
+# Zapis do pliku Excel
+data_combined.to_excel(excel_file_path, index=False)
